081bfab06277e37a28107ef1ab6352d7d42bdbac
Steve <steve@Steves-MacBook-Pro.local>
2024-09-27 15:01:54 -0400

    first commit

---
+++ document_uploader.py
@@ -0,0 +1,46 @@
+from global_settings import STORAGE_PATH, CACHE_FILE
+from logging_functions import log_action
+from llama_index import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core.ingestion import IngestionPipeline, IngestionCache
+from llama_index.core.node_parser import TokenTextSplitter
+from llama_index.core.extractors import SummaryExtractor
+from llama_index.embeddings.openai import OpenAIEmbedding
+
+def ingest_documents():
+    documents = SimpleDirectoryReader(
+        STORAGE_PATH,
+        filename_as_id=True
+    ).load_data()
+    for doc in documents:
+        print(doc.id)
+        log_action(
+            f"File'{doc.id_}'uploaded_user",
+            action_type="UPLOAD"
+        )
+
+    try:
+        cached_hashes = IngestionCache.from_persist_path(
+            CACHE_FILE
+        )
+        print("Cache file found. Running using cache..")
+    except:
+        cached_hashes = ""
+        print("No cache file found. Running without..")
+    pipeline = IngestionPipeline(
+        transformations=[
+            TokenTextSplitter(
+                chunk_size=1024,
+                chunk_overlap=20
+            ),
+            SummaryExtractor(summaries=['self']),
+            OpenAIEmbedding()
+        ],
+        cache=cached_hashes
+    )
+    nodes = pipeline.run(documents=documents)
+    pipeline.cache.persist(CACHE_FILE)
+
+    return nodes
+
+if __name__ == "__main__":
+    embedded_nodes = ingest_documents()
---
+++ quiz_builder.py
@@ -0,0 +1,56 @@
+from llama_index.core import load_index_from_storage, StorageContext
+from llama_index.core.program import DFRowsProgram
+from llama_index.program.openai import OpenAIPydanticProgram
+from global_settings import INDEX_STORAGE, QUIZ_SIZE, QUIZ_FILE
+import pandas as pd
+
+def build_quiz(topic):
+    # Data Frame to sturcture the quiz and their associated options and answers.
+    df = pd.DataFrame({
+        "Question_no":pd.Series(dtype="int"),
+        "Question_text":pd.Series(dtype="istr"),
+        "Option1":pd.Series(dtype="str"),
+        "Option2":pd.Series(dtype="str"),
+        "Option3":pd.Series(dtype="str"),
+        "Option4":pd.Series(dtype="str"),
+        "Correct_answer":pd.Series(dtype="str"),
+        "Rational":pd.Series(dtype="str"),
+    })
+    # Load vector index from storage.
+    storage_context = StorageContext.from_defaults(
+        persist_dir=INDEX_STORAGE
+    )
+    vector_index = load_index_from_storage(
+        storage_context, index_id="vector"
+    )
+    #
+    df_rows_program = DFRowsProgram.from_defaults(
+        pydantic_program_cls=OpenAIPydanticProgram,
+        df=df,
+    )
+    # Query engine
+    query_engine = vector_index.as_query_engine()
+    # The prompt
+    quiz_query = (
+        f"Create {QUIZ_SIZE} different quiz "
+        "questions relevant for testing "
+        "a candidate's knowledge about "
+        f"{topic}. Each question will have 4 "
+        "answer options. Questions must be "
+        "general topic-related, not specific "
+        "to the proivided text. For each "
+        "question, provide also the correct "
+        "answer and the answer rationale. "
+        "The rationale must not make any "
+        "reference to the provided context "
+        "any exams or the topic name. Only "
+        "one answer optionshould be correct."
+    )
+    # Prompt passed to query engine to get response.
+    response = query_engine.query(quiz_query)
+    # Response is processed to convert it into a DataFrame format.
+    result_obj = df_rows_program(input_str=response)
+    new_df=result_obj.to_df(existing_df=df)
+    # DataFrame is converted to a CSV file.
+    new_df.to_csv(QUIZ_FILE, index=False)
+    return new_df
